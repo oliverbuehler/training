1cc6032c66d8325582c81228d6ea69c26464168d
oliver <oliver.buehler@agility-informatik.ch>
2015-08-19 03:27:02 +0200

    training jpa

--- ch.agility.training.jpa/src/test/java/ch/agility/training/jpa/TestJPA.java
+++ ch.agility.training.jpa/src/test/java/ch/agility/training/jpa/TestJPA.java
@@ -4,8 +4,10 @@ import java.sql.SQLException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.LockModeType;
 import javax.persistence.Persistence;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import ch.agility.training.jpa.entity.Master;
@@ -32,6 +34,35 @@ public class TestJPA extends DatabaseTest {
 				DatabaseHelper.executeSqlQuery("select * from jpa.master"));
 
 		Master reloaded = em.find(Master.class, masterEntity.getId());
+		System.out.println(reloaded.getStringAttribute());
+
+		em.close();
+		emf.close();
+	}
+
+	@Test
+	@Ignore
+	public void testPersistenceLockModes() throws SQLException {
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("ch.agility.training.jpa");
+		EntityManager em = emf.createEntityManager();
+
+		System.out.println(DatabaseHelper.executeSqlQuery("show schemas"));
+		System.out.println(DatabaseHelper.executeSqlQuery("show tables"));
+
+		Master masterEntity = new Master();
+		masterEntity.setStringAttribute("string1");
+
+		em.getTransaction().begin();
+		em.persist(masterEntity);
+		em.getTransaction().commit();
+		System.out.println(masterEntity.getId());
+		System.out.println(
+				DatabaseHelper.executeSqlQuery("select * from jpa.master"));
+
+		// TODO Oliver test locking
+		Master reloaded = em.find(Master.class, masterEntity.getId(),
+				LockModeType.PESSIMISTIC_READ);
 		System.out.println(reloaded.getStringAttribute());
 
 		em.close();
